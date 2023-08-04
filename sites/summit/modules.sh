370a0ee7804da3245a180c596029dea67988fffa
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-08-04 16:18:20 -0500

    intial commit for the outflow-forcing archive

---
+++ sites/summit/modules.sh
@@ -0,0 +1,13 @@
+# Load GCC and python module
+module load gcc/9.1.0
+
+#module load python/3.8-anaconda3
+module load openblas/0.3.17-omp
+module load python/3.8.10
+
+# Load HDF5 module in desired configuration if available. If not specified
+# the HDF5 will be built when setting up software
+module load hdf5/1.10.7
+
+# load git-lfs
+module load git-lfs
