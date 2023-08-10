f83afff84224f78019c1bb83b140d0b5e2c27ed5
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-08-10 08:52:53 -0500

    add config

---
+++ config.sh
@@ -0,0 +1,18 @@
+# Site directory, refer to sites/README.md for details
+SiteName="sedona"
+
+# AMReX tag, set it to false once software/ is setup
+AMReX_Enable=true
+AMReX_TAG="cff96a9"
+
+# Flash-X tag, set it to flashe once software/ is setup
+FlashX_Enable=true
+FlashX_TAG="main"
+
+# FlashKit for post processing
+FlashKit_Enable=true
+FlashKit_TAG="364c99d"
+
+# HDF5 tag, set if HDF5 is setup
+HDF5_Enable=false
+HDF5_TAG="93754ca"
